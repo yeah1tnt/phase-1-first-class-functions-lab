65a326d359db5791e77590d40973b0c178916a73
Tong Tang <pichip22@hotmail.com>
2023-03-18 11:54:22 -0400

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+/*function returnFirstTwoDrivers(array){
+    let array2 = array.slice(0,2);
+    return array2;
+}*/
+const returnFirstTwoDrivers = array => array.slice(0,2);
+
+/*function returnLastTwoDrivers(array){
+    let array2 = array.slice(-2);
+    return array2;
+}*/
+const returnLastTwoDrivers = array => array.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+/*function createFareMultiplier(num){
+    return function(mul){
+        return num*mul};
+}*/
+const createFareMultiplier = num => mul => num*mul;
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(array, drivers){
+    return drivers(array);
+}
